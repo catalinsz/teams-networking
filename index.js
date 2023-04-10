615439337352d2ce8f62e0040d799729e1c2ddb6
catalinszilagyi <szilagyi.catalin@yahoo.com>
2023-04-10 19:48:08 +0300

    made post

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ function getTeamsRequest() {
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((r) => {
-    return r.json();
-  });
+  }).then((r) => r.json());
+}
+
+function createTeamsRequest(team) {
+  return fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(team),
+  }).then((r) => r.json());
 }
 
 function getTeamsAsHTML(team) {
@@ -16,15 +24,43 @@ function getTeamsAsHTML(team) {
         <td>${team.members}</td>
         <td>${team.name}</td>
         <td>${team.url}</td>
+        <td></td>
     </tr>
     `;
 }
 
 function showTeams(teams) {
   const html = teams.map(getTeamsAsHTML);
-  document.querySelector("table tbody").innerHTML = html.join("");
+  $("table tbody").innerHTML = html.join("");
+}
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  const promotion = $("#promotion").value;
+  const members = $("#members").value;
+  const projectName = $("#projectName").value;
+  const projectUrl = $("#projectUrl").value;
+
+  const team = {
+    promotion: promotion,
+    members: members,
+    name: projectName,
+    url: projectUrl,
+  };
+  createTeamsRequest(team).then(() => window.location.reload());
+}
+
+function initEvents() {
+  $("#editForm").addEventListener("submit", formSubmit);
 }
 
 getTeamsRequest().then((teams) => {
   showTeams(teams);
 });
+
+initEvents();
